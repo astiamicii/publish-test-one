da0513f16f061348a0b35eb315640a3e45c82c68
Alex Ciobica <alex.ciobica@gmail.com>
2020-05-19 14:32:21 +0300

    improvement: something

--- packages/module/index.js
+++ packages/module/index.js
@@ -1,21 +1,22 @@
-const log = () => 'some meaningful message!';
+const log = () => "some meaningful message!";
 
-const log2 = () => 'some meani;wlq;lwqpqpwngful message? 21.';
+const log2 = () => "some meani;wlq;lwqpqpwngful message? 21.";
 
 const log3 = () => {
-  console.log('sful @@21231340340@!!!!.333. 32ssssss122222222');
+  console.log("sful @@21231340340@!!!!.333. 32ssssss122222222");
 };
 
 const log14 = () => {
-}
+  // Something
+};
 
 const log4 = () => {
-  console.log('some meaningsdadasful messssssage33?!..!!45www693');
-  console.log('double log');
+  console.log("some meaningsdadasful messssssage33?!..!!45www693");
+  console.log("double log");
 };
 
 const log5 = () => {
-  console.log('some meaningful messssssa333ge33?!..!!45www693@#!');
+  console.log("some meaningful messssssa333ge33?!..!!45www693@#!");
 };
 
 module.exports = {
